0ace1dc2a6c6d17ee9b34870c68cc5da1b3726ca
Mathilde <mathilde.jolly@kokoroe.fr>
2020-12-16 16:42:35 +0100

    Default data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Profile } from "../models/profile";
 import "./App.scss";
+import User from "./user/User"
 
-export class App extends React.Component<{}> {
-  public componentDidMount() {
-    if (chrome && chrome.tabs) {
-      chrome.tabs.query({ currentWindow: true, active: true }, tabs => {
-        const tab = tabs[0];
-        chrome.tabs.sendMessage(tab.id || 0, { from: "popup", subject: "getFullName" }, response => {
-          console.log(response);
-        });
-      });
+interface State extends Profile {}
+
+const App: React.FunctionComponent<{}> = () => {
+  const [fullName, setFullName] = useState("Stephane");
+  const [title, setTitle] = useState("Architecte");
+  const [country, setCountry] = useState("New York");
+  const [imageUrl, setImage] = useState("https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png");
+  useEffect(() => {
+    if(chrome &&chrome.tabs){
+      chrome.tabs.query({ currentWindow: true, active: true}, tabs => {
+        const tab = tabs[0]
+        chrome.tabs.sendMessage(tab.id || 0, {from: "popup", subject: "getFullName"}, response => {
+          setFullName(response.fullName);
+          setTitle(response.title)
+          setCountry(response.country)
+          setImage(response.imageUrl)
+        })
+      })
     }
-  }
-
-  render() {
-    return <div className="app">Hello world</div>;
-  }
+  })
+  return (
+    <div className="app">
+      <User fullName={fullName} title={title} country={country} imageUrl={imageUrl}></User>
+    </div>
+  );
 }
 
 export default App;
